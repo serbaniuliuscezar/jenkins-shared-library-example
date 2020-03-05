fdf004786b35fd94d5bf67e5eab713fac50ddfb3
Serban Cezar <iuserban@adobe.com>
2020-03-05 17:32:57 +0200

    Test

--- src/org/somecompany/deploy/Deploy.groovy
+++ src/org/somecompany/deploy/Deploy.groovy
@@ -1,4 +1,4 @@
-package org.somecompany.build
+package org.somecompany.deploy
 
 import org.somecompany.IStepExecutor
 import org.somecompany.ioc.ContextRegistry
@@ -6,17 +6,17 @@ import org.somecompany.ioc.ContextRegistry
 /**
  * Example class (without proper implementation) for using the MsBuild tool for building .NET projects.
  */
-class MsBuild implements Serializable {
-    private String solutionPath
+class Deploy implements Serializable {
+    private String _solutionPath
 
-    MsBuild(String solutionPath) {
-        this.solutionPath = solutionPath
+    Deploy(String solutionPath) {
+        _solutionPath = solutionPath
     }
 
     void build() {
         IStepExecutor steps = ContextRegistry.getContext().getStepExecutor()
 
-        int returnStatus = steps.sh("echo \"building ${this.solutionPath}...\"")
+        int returnStatus = steps.sh("echo \"building ${this._solutionPath}...\"")
         if (returnStatus != 0) {
             steps.error("Some error")
         }
